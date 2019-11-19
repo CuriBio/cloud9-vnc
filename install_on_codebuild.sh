177a0ddabce53a26c0c86fd7de114250760b50c5
Eli <ubuntu@ip-172-31-92-57.ec2.internal>
2019-11-19 11:42:20 -0500

    added install script now using sudo for AWS codebuild

---
+++ install_on_codebuild.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Removes sudo and sets default password for being installed during a AWS CodeBuild run for CI
+
+#Request sudo permissions
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+
+
+# Check prerequisites
+apt-get update
+apt-get install -y supervisor xvfb fluxbox x11vnc websockify
+
+
+#Clone noVNC into proper /opt/ directory
+echo "Cloning noVNC..."
+echo
+
+{
+    git clone git://github.com/kanaka/noVNC /opt/noVNC/
+}&> /dev/null
+
+#Copy supervisord configuration to proper configuration directory
+echo "Configuring supervisord..."
+echo
+
+cp supervisord.conf ${HOME}/.config/supervisord.conf
+
+
+#Create the proper directory for the script
+echo "Install run script..."
+echo
+
+mkdir -p /opt/c9vnc
+
+#Copy the run script to proper /opt/ directory
+\cp run.sh /opt/c9vnc/c9vnc.sh
+
+
+#Symlink script
+{
+    ln -s /opt/c9vnc/c9vnc.sh /usr/local/bin/c9vnc
+}&> /dev/null
+
+#Export X11 Settings
+echo "Configuring X11"
+echo
+
+mkdir -p /tmp/X11
+echo export XDG_RUNTIME_DIR=/tmp/C9VNC >> ~/.bashrc
+echo export DISPLAY=:99.0 >> ~/.bashrc
+source ~/.bashrc
+
